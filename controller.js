8d7819376f3d86952770b402f172df935bbd44a8
onico100 <142163577+onico100@users.noreply.github.com>
2024-10-10 09:06:52 +0300

    add side bar for new button

---
+++ controller.js
@@ -0,0 +1,7 @@
+function openSidebar() {
+  document.getElementById("newBookSidebar").style.width = "400px";
+}
+
+function closeSidebar() {
+  document.getElementById("newBookSidebar").style.width = "0";
+}
