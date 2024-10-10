48e756fe13f37530e8affc3bcb69a76d52af03db
onico100 <142163577+onico100@users.noreply.github.com>
2024-10-10 11:15:48 +0300

    add post

--- controller.js
+++ controller.js
@@ -32,3 +32,35 @@ function goPrevious() {
 function clearBook() {
   renderClearBookBoard();
 }
+
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Get values from form inputs
+  const bookId = document.getElementById("bookId").value;
+  const bookName = document.getElementById("bookName").value;
+  const bookPrice = document.getElementById("bookPrice").value;
+  const bookImg = document.getElementById("bookImg").value;
+  const bookRating = document.getElementById("bookRating").value;
+
+  // Validate inputs
+  if (!bookId || !bookName || !bookPrice || !bookImg) {
+    alert("Please fill out all fields");
+    return;
+  }
+
+  // Create a new book object
+  const newBook = {
+    id: parseInt(bookId),
+    title: bookName,
+    price: parseFloat(bookPrice),
+    coverImageUrl: bookImg,
+    rating: bookRating,
+  };
+
+  // Add the new book to the array
+  gBookData.push(newBook);
+
+  //reset the form
+  alert("Book added successfully!");
+}
